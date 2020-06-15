f1a4597c4eb8172987932103fc4c0d26834779de
HHinkel <hhinkel15@gmail.com>
2020-06-14 20:32:28 -0400

    Initial Commit

---
+++ Progress Journal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Progress Journal
+//
+//  Created by XCodeClub on 2020-06-14.
+//  Copyright © 2020 Have a Good Ride Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
